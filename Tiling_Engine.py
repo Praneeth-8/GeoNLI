bfbefb4cf9475cdf90f11b953d051ecd6c2e2cfe
Praneeth <badripraneeth785@gmail.com>
2026-07-16 10:29:02 +0530

    overlap added

--- Tiling_Engine.py
+++ Tiling_Engine.py
@@ -4,13 +4,25 @@ IMAGE_PATH=""
 
 
 img = cv2.imread(IMAGE_PATH) # numpy array of the image
-def generate_tiles(img,height,width):
+def generate_tiles(img,tile_h,tile_w,overlap=0):
     img_height=img.shape[0]
     img_width=img.shape[1]
+    stride_h, stride_w = tile_h - overlap,tile_w - overlap # overlap in pixels
     tiles=[]
 
-    for i in range(img_height//height):
-        for j in range(img_width//width):
-            tiles.append((img[i*height:(i+1)*height,j*width:(j+1)*width,],(i,j)))
-        
+    y=0
+
+    while y < img_height:
+        x = 0
+        while x < img_width:
+            y_end, x_end = min(y+tile_h,img_height),min(x+tile_w,img_width) # for bounds
+            tile = img[y:y_end, x:x_end]
+            tiles.append({"tile": tile, "x":x, "y":y})
+            if x_end == img_width:
+                break
+            x+=stride_w
+        if y_end == img_height:
+            break
+        y += stride_h
+
     return tiles
